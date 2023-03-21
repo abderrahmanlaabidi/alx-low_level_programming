f3db9bdead26b9151fb33a287eed55d451bb203d
abderrahmanlaabidi <abderrahman.laabidi@gmail.com>
2023-03-20 17:05:45 -0700

    #assembler

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* Main – Determines if a number is positive, negative or zero.
+* 
+* Return: Always 0 ‘Success)
+*/
+Int main(void)
+{  int n;
+Srand(time(0))
+n = rand() – RAND_MAX / 2 ;
+if (n > 0 )
+{
+	Printf (“%d is positive\n” , n);
+}
+Else if (a == 0)
+{
+
+	Prontf (“%d is is zero\n” , n);
+}
+Else 
+{
+	Printf (“%d is is negative\n” , n);
+}
+Return (0);
+
+}
+	
