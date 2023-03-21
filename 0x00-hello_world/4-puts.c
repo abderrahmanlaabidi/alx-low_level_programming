81cb4acfefb3b71879b8f734cc5a7469c12209d2
abderrahmanlaabidi <abderrahman.laabidi@gmail.com>
2023-03-20 17:29:12 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
-# include <studio.h>
-/**
-/* main - Aprogram that pront a line with puts function
- * Return: 0 (Success)
- * /
- * int main( void)
- * {
- * puts(""Programming is like building a multilingual puzzle")
+
+ int main( void)
+  {
+ puts("\'Programming is like building a multilingual puzzle"):
+ return  (0):
+  }
